2c875584216d6a4df6806daefbf2b1d174a3b7e2
fjoys <fjoys@JOYSTON>
2023-11-28 11:29:22 +0530

    second commit

---
+++ src/test/java/maven/jenkins/TestScript56Test.java
@@ -0,0 +1,17 @@
+package maven.jenkins;
+
+import org.testng.annotations.Test;
+
+public class TestScript56Test {
+
+	@Test(groups="smoke")
+	public void t5() {
+		System.out.println("-------TS5-------");
+	}
+
+	@Test(groups="regression")
+	public void t6() {
+		System.out.println("-------TS6-------");
+	}
+
+}
