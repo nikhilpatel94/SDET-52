e2c2074423235e8ae37a0d83b32fa602dbe5a0ae
nikhilpatel <nikhilpatel@localhost>
2023-11-28 14:55:43 +0530

    hi

---
+++ src/test/java/maven/jenkins/hello.java
@@ -0,0 +1,17 @@
+package maven.jenkins;
+
+import org.testng.annotations.Test;
+
+public class hello {
+	@Test
+		public void tetsMethod() {
+			System.out.println("hbjds");
+	
+			System.out.println("hi");
+			System.out.println("hi");
+			System.out.println();
+	
+			System.out.println("hi");
+		}
+}
+
