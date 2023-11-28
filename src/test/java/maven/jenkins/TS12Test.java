203e48e9807d398f155e3db62cdbc75a040e0119
nikhilpatel <nikhilpatel@localhost>
2023-11-28 11:01:19 +0530

    Design is completed

---
+++ src/test/java/maven/jenkins/TS12Test.java
@@ -0,0 +1,20 @@
+package maven.jenkins;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class TS12Test {
+	
+	@Test(groups="smoke")
+	public void t1() {
+		System.out.println("-------TS1-------");
+	}
+
+	@Test(groups="regression")
+	public void t2() {
+		System.out.println("-------TS2-------");
+	}
+
+}
---
+++ src/test/java/maven/jenkins/TS34Test.java
@@ -0,0 +1,19 @@
+package maven.jenkins;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class TS34Test {
+	@Test(groups="smoke")
+	public void t3() {
+		System.out.println("-------TS3-------");
+	}
+
+	@Test(groups="regression")
+	public void t4() {
+		System.out.println("-------TS4-------");
+	}
+
+}
